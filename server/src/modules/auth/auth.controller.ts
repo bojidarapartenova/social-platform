49df7efbf75a09747674ea511c4cec0bea1712d7
bojidarapartenova <bpartenova@gmail.com>
2026-07-09 13:52:30 +0300

    edit auth service and controller

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,27 +1,28 @@
 import { Request, Response } from "express";
-import { registerUser } from "./auth.service";
+import { AuthService } from "./auth.service";
+import { error } from "node:console";
 
-export async function register(
-    req: Request,
-    res: Response
-) {
+const authService = new AuthService();
+
+export async function register(req: Request, res: Response) {
     try {
         const { username, email, password } = req.body;
-
-        const user = await registerUser(
-            username,
-            email,
-            password
-        );
-
-        res.status(201).json({
-            message: "User created",
-            user
-        });
-
-    } catch (error: any) {
-        res.status(400).json({
-            message: error.message
-        });
+        const user = await authService.registerUser(username, email, password);
+        res.status(201).json({ message: "User created", user });
+    }
+    catch (error: any) {
+        res.status(400).json({ message: error.message });
     }
 }
+
+export async function login(req: Request, res: Response) {
+    try {
+        const { email, password } = req.body;
+        const { token, user } = await authService.loginUser(email, password);
+        res.status(200).json({ message: "Login successful", token, user });
+    }
+    catch (error: any) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -1,14 +1,42 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { UserRepository } from "../users/user.repository";
+import { env } from "../../config/env";
+import { Error } from "mongoose";
 
 export class AuthService {
     constructor(private userRepo: UserRepository = new UserRepository()) { }
 
     async registerUser(username: string, email: string, password: string) {
         const existingUser = await this.userRepo.findOne({ email });
-        if (existingUser) throw new Error("Email already exists");
+
+        if (existingUser) {
+            throw new Error("Email already exists");
+        }
 
         const password_hashed = await bcrypt.hash(password, 10);
+
         return this.userRepo.create({ username, email, password: password_hashed });
+    }
+
+    async loginUser(email: string, password: string) {
+        const user = await this.userRepo.findOne({ email });
+
+        if (!user) {
+            throw new Error("Invalid email");
+        }
+
+        const isMatch = await bcrypt.compare(user.password, password);
+        if (!isMatch) {
+            throw new Error("Invalid password");
+        }
+
+        const token = jwt.sign(
+            { userId: user._id, role: user.role },
+            env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        return { token, user };
     }
 }
